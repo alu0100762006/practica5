08e0f59110acb112394bba1f51b4a4748f244d43
Alexandra Esplugas Sabina <alu0100762006@ull.edu.es>
2016-10-28 11:02:54 +0000

    Corrigiendo errores del codigo

--- test/tc_receta.rb
+++ test/tc_receta.rb
@@ -1,20 +1,22 @@
 #Aqui se realizan las pruebas para comprobar que la clase Tortitas funciona
+require 'lib/receta'
+require 'test/unit'
 
-class TestReceta
+class TestReceta < Test::Unit::TestCase
     
     def setup
-        @tortitas = Receta.new("tortitas")
+        #@tortitas = Receta.new("tortitas")
         
         titulo = "tortitas"
-        porciones = 1
-        tiempo = 10
+        porciones = "1"
+        tiempo = "10 min"
         ing = %w{huevos, harina, leche}
         pasos = %w{unir, batir, freir}
         @tortitas = Receta.new(titulo, porciones, tiempo, ing, pasos)
     end
     
     def test_simple
-        assert_equal("tortitas", @tortitas.to_s)
+        assert_equal("tortitas\n====\n", @tortitas.to_s)
     end
     
 end
